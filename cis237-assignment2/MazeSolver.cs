cbf864284829bbd5db1ec3fadf510983728cba16
Edrick Tamayo <edrick.tamayo@zoho.com>
2020-10-01 02:34:28 -0400

    Add code to output maze and recursive code to (try) to solve maze

--- cis237-assignment2/MazeSolver.cs
+++ cis237-assignment2/MazeSolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace cis237_assignment2
 {
@@ -18,8 +19,23 @@ namespace cis237_assignment2
         public void SolveMaze(char[,] maze, int xStart, int yStart)
         {
             // Do work needed to use mazeTraversal recursive call and solve the maze.
+            Console.WriteLine(Environment.NewLine + "Solve Maze");
+            MazeTraversal(maze, xStart, yStart);
         }
 
+        // Outside Reference: https://stackoverflow.com/questions/2044591/what-is-the-difference-between-array-getlength-and-array-length#:~:text=GetLength(0)%20method%20returns%20number,direction%20in%20a%20multidimensional%20array.
+        private void PrintMaze(char[,] maze)
+        {
+            for (int i = 0; i < maze.GetLength(0); i++)
+            {
+                for (int t = 0; t < maze.GetLength(1); t++)
+                {
+                    Console.Write(maze[i, t] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(Environment.NewLine);
+        }
 
         /// <summary>
         /// This should be the recursive method that gets called to solve the maze.
@@ -28,9 +44,51 @@ namespace cis237_assignment2
         /// More than likely you will need to pass in at a minimum the current position
         /// in X and Y maze coordinates. EX: mazeTraversal(int currentX, int currentY)
         /// </summary>
-        private void mazeTraversal()
+        // Outside Reference: https://www.cs.bu.edu/teaching/alg/maze/
+        private void MazeTraversal(char[,] maze, int xStart, int yStart)
         {
+            maze[xStart, yStart] = char.Parse("O");
+            PrintMaze(maze);
             // Implement maze traversal recursive call
+            if (xStart + 1 == maze.GetLength(0) || yStart + 1 == maze.GetLength(1) + 1)
+            {
+                Console.WriteLine("Finish!");
+            }
+            else if (maze[(xStart - 1), yStart] == '.')
+            {
+                xStart = xStart - 1;
+                maze[xStart, yStart] = char.Parse("O");
+                PrintMaze(maze);
+                MazeTraversal(maze, xStart, yStart);
+            }
+            else if (maze[(xStart + 1), yStart] == '.')
+            {
+                xStart = xStart + 1;
+                maze[xStart, yStart] = char.Parse("O");
+                PrintMaze(maze);
+                MazeTraversal(maze, xStart, yStart);
+            }
+            else if (maze[xStart, (yStart - 1)] == '.')
+            {
+                yStart = yStart - 1;
+                maze[xStart, yStart] = char.Parse("O");
+                PrintMaze(maze);
+                MazeTraversal(maze, xStart, yStart);
+            }
+            else if (maze[xStart, (yStart + 1)] == '.')
+            {
+                yStart = yStart + 1;
+                maze[xStart, yStart] = char.Parse("O");
+                PrintMaze(maze);
+                MazeTraversal(maze, xStart, yStart);
+            }
+            else if (maze[(xStart - 1), yStart] != '.' && //maze[(xStart - 1), yStart] != '.' &&
+                     maze[xStart, (yStart - 1)] != '.' && //maze[xStart, (yStart - 1)] != '.' && 
+                     maze[(xStart + 1), yStart] != '.' && //maze[(xStart + 1), yStart] != '.' &&
+                     maze[xStart, (yStart + 1)] != '.') //&& maze[xStart, (yStart + 1)] != '.')
+            {
+                maze[xStart, yStart] = char.Parse("X");
+            }
         }
     }
 }
